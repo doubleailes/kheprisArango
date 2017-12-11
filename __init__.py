c92b9be39c5ade47b135c41d06c08c5a591a55df
Doubleailes <philippe.llerena@gmail.com>
2017-12-11 22:54:29 +0100

    Change vertex / edge

--- __init__.py
+++ __init__.py
@@ -1,43 +1,47 @@
 class Materiel(dict):
 	"""docstring for Materiel"""
-	def __init__(self, id, active,use):
+	def __init__(self, id_, active,use):
 		super(Materiel, self).__init__()
-		self['Id'] = id
+		self['id_'] = id_
 		self['Active'] = active
 		self['Use'] = use
 
 class MotherBoard(Materiel):
-	def __init__(self, id, active,use,ramSlot,cpu=None):
-		Materiel.__init__(self, id, active,use)
+	def __init__(self, id_, active,use,ramSlot,cpu=None):
+		Materiel.__init__(self, id_, active,use)
 		self['RamSlot'] = ramSlot
 		self['Cpu']= cpu
 
 
 class Chassis(Materiel):
-	def __init__(self, id, active,use,nodeCount=4,powerSupply=0):
-		Materiel.__init__(self, id, active,use)
+	def __init__(self, id_, active,use,nodeCount=4,powerSupply=0):
+		Materiel.__init__(self, id_, active,use)
 		self['NodeCount'] = nodeCount
 		self['PowerSupply'] = powerSupply
 
 class Switch(Materiel):
-	def __init__(self, id, active,use):
-		Materiel.__init__(self, id, active,use)
+	def __init__(self, id_, active,use):
+		Materiel.__init__(self, id_, active,use,port)
+		self['port'] = port
+
+class Customer(dict):
+	def __init__(self,id_,name):
+		self['id_'] = id_
+		self['name'] = name
 
 class Action(dict):
 	"""docstring for Action"""
-	def __init__(self,_id,_source,_target ):
+	def __init__(self,_id_,_source,_target ):
 		super(Action, self).__init__()
-		self['_id'] = _id
+		self['_id_'] = _id_
 		self['Source'] = _source
 		self['Target'] = _target
-		
 
 class Setup(Action):
 	"""docstring for Setup"""
 	def __init__(self, arg):
 		self.arg = arg
 		
-
 class Leasing(Action):
 	"""docstring for Leasing"""
 	def __init__(self, arg):
